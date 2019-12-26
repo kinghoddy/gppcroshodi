aa5d57e610e7832cf2d2d9d9c790887e81df8778
kinghoddy git config --global user.email odunmilade@gmail.com <you@example.com>
2019-12-26 23:23:20 +0100

    chamges

--- js/gallery.js
+++ js/gallery.js
@@ -1,103 +1,146 @@
-var main = document.querySelector('.main');
-function singlePage() {
-
-    var loader = `<img src="../img/load.png" alt="loader" class="loader">`,
-        fragCache = {};
-    // setting inner html on page load
-    navigate();
-    // setting innerHTML on url change
-    window.addEventListener('hashchange', navigate)
-
-
-    function fetchData(path, callBack) {
-        // create new ajax request
-        var request = new XMLHttpRequest();
-        request.onload = () => {
-            callBack(request.responseText);
-        }
-        request.open('GET', path)
-        request.send(null)
+var src = null;
+var sources = {
+    img : [
+        '../img/carol19/carol-pot.jpg',
+        '../img/carol19/carol-land.jpg',
+        '../img/carol19/carol (1).jpg',
+        '../img/carol19/carol (2).jpg',
+        '../img/carol19/carol (3).jpg',
+        '../img/carol19/carol (4).jpg',
+        '../img/apata2.jpg'
+],
+    vid : [
+        '../vid/carol.mp4',
+        '../vid/croud.mp4'
+    ]
+}
+const getSrc = type => {
+    let n ;
+    if (type == 'img'){
+     n = Math.floor(Math.random() * sources.img.length);
+     src = sources.img[n];
+    }else {
+        n = Math.floor(Math.random() * sources.vid.length);
+        src = sources.vid[n];
     }
-    function getContent(fragmentId, callBack) {
-        // if page has been fetched before
-        if (fragCache[fragmentId]) {
-            callBack(fragCache[fragmentId])
-        } else {
-            fetchData(`frag/${fragmentId}frag.html`, (content) => {
-                fragCache[fragmentId] = content;
-                callBack(content)
-            })
-        }
-    }
-    function setActiveClass(fragmentId) {
-        var links = document.querySelectorAll('.header-links'), pageName;
-        Array.from(links).forEach(a => {
-            pageName = a.getAttribute('href').substr(1);
-            console.log(pageName)
-            if (pageName == fragmentId) {
-                a.parentElement.classList.add('active')
-            } else {
-                a.parentElement.classList.remove('active');
-            }
-        })
-    }
-    function playBack() {
-        var dom = document.getElementById('vidMain'), dom = document.getElementById('videoDOM'),
-            videos = Array.from(document.querySelectorAll('.video-list video'));
-        videos.forEach(cur => {
-            console.log(cur);
-            cur.addEventListener('click', () => {
-                alert('hi')
-                dom.classList.remove('d-none')
-                vidMain.src = cur.src
-            })
-        });
+    return src
+}
 
-        // if (location.hash == '#photo') {
-        //     dom.classList.add('d-none')
-        // }
+const carouselItem = type =>{
+    let value = ` <div class="carousel-item active">
+    <img src="${getSrc('img')}" alt="${src}">
+               </div>`;
+    if (type == 'img'){
+        for (var i = 0; i < sources.img.length; i++){
+                 value += ` <div class="carousel-item">
+                               <img src="${getSrc('img')}" alt="${src}">
+                            </div>`;
+        }
+    }else {
+        for (var i = 0; i < sources.vid.length; i++){
+            value += ` <div class="carousel-item">
+                          <video src="${getSrc('vid')}" muted autoplay></video>
+                       </div>`;
+        }
     }
-    function navigate() {
-        var fragmentId = location.hash.substr(1);
-        main.innerHTML = loader;
-        if (!location.hash) {
-            location.hash = '#photo'
-        }
-        if (fragmentId) {
-            getContent(fragmentId, content => {
-                main.innerHTML = content;
-                $(".parent-container").magnificPopup({
-                    delegate: "a",
-                    type: "image",
-                    // removalDelay: 3000,
-                    mainClass: 'mfp-fade',
-                    gallery: {
-                        enabled: true,
-                        preload: [5, 5]
-                    }
-                });
-                playBack();
-            })
+    return value
+}
 
+const miniCarouselItem = type =>{
+    var value = ''
+    if (type == 'img'){
+         value = ` <div class="carousel-item active">
+                             <img src="${getSrc('img')}" alt="${src}">
+                      </div>`;
+        for (var i = 0; i < sources.img.length; i++){
+                 value += ` <div class="carousel-item">
+                               <img src="${sources.img[i]}" alt="${src}">
+                            </div>`;
         }
-        setActiveClass(fragmentId);
+    }else {
+         value = ` <div class="carousel-item active">
+                       <video src="${getSrc('vid')}" autoplay muted></video>
+                    </div>`;
+        for (var i = 0; i < sources.vid.length; i++){
+            value += ` <div class="carousel-item">
+                          <video src="${sources.vid[i]}" ></video>
+                       </div>`;
+        }
+    }
+    return value
+}
+
+const carousel = type =>{
+    if (type == 'img'){
+        return (
+            `  <div id="carouselId0" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                     ${carouselItem('img')}
+            </div>
+            <a class="carousel-control-prev" href="#carouselId0" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselId0" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+          </div>`
+        )
+    }else if (type == 'vid'){
+     return (
+        `  <div id="carouselId0" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+                 ${carouselItem('vid')}
+        </div>
+        <a class="carousel-control-prev" href="#carouselId0" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselId0" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+      </div>`
+     )
+    }else if(type == 'miniImg'){
+        return (
+            `  <div id="carouselId1" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                     ${miniCarouselItem('img')}
+            </div>
+            <a class="carousel-control-prev" href="#carouselId1" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselId1" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+          </div>`
+         )
+    }else if (type == 'miniVid'){
+        return (
+            `  <div id="carouselId2" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                     ${miniCarouselItem('vid')}
+            </div>
+            <a class="carousel-control-prev" href="#carouselId2" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselId2" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+          </div>`
+         )
     }
 }
 
+(function init (){
+    document.querySelector('.con').innerHTML = carousel('img');
+    document.querySelector('.imgcon').innerHTML = carousel('miniImg');
+    document.querySelector('.vidcon').innerHTML = carousel('miniVid');
 
-(function init() {
-    var nav = document.querySelector('nav');
-    nav.addEventListener('click', () => {
-        nav.children[1].classList.toggle('blind')
-    })
-
-    window.addEventListener('scroll', () => {
-        var category = document.querySelector('.header-top'),
-            db = document.documentElement;
-        if (db.scrollTop > 100) category.style = `background: #111;
-                                                       top:0;
-                                                       padding:0`;
-        else category.style = '';
-    })
-    singlePage()
-})()
+ }())   
